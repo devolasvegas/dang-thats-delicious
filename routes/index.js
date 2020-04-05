57e8b56fa21858de968168ce32701b19de71fc13
devolasvegas <devondaviau@yahoo.ca>
2020-04-05 14:26:00 -0400

    a couple examples using response send and json methods

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+router.get("/", (req, res) => {
+  // res.send("Hey! It works!");
+
+  res.json(req.query);
+});
+
+router.get("/reverse/:name/:last/:age", (req, res) => {
+  const reverse = [...req.params.name].reverse().join("");
+  res.send(reverse);
 });
 
 module.exports = router;
