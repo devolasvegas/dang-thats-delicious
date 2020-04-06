07dde4821f4e0b5e751e5c883d594938609da1f7
devolasvegas <devondaviau@yahoo.ca>
2020-04-06 14:47:28 -0400

    Create store schema

--- models/Store.js
+++ models/Store.js
@@ -16,4 +16,16 @@ const storeSchema = new mongoose.Schema({
   tags: [String],
 });
 
+storeSchema.pre("save", function (next) {
+  // create new slug only if adding new name or modifying name/
+  if (!this.isModified("name")) {
+    return next();
+  }
+
+  this.slug = slug(this.name);
+  next();
+
+  // TODO: add code to eliminate redundant slugs
+});
+
 module.exports = mongoose.model("Store", storeSchema);
