9fa69716377ca49789d2de077ee300ffca20a7a4
devolasvegas <devondaviau@yahoo.ca>
2020-04-08 15:31:27 -0400

    add address and coordinates to the schema, add fields for these to the storeform

--- models/Store.js
+++ models/Store.js
@@ -1,31 +1,51 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-const slug = require("slugs");
+const slug = require('slugs');
 
 const storeSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: "Please enter a store name!",
-  },
-  slug: String,
-  description: {
-    type: String,
-    trim: true,
-  },
-  tags: [String],
+	name: {
+		type: String,
+		trim: true,
+		required: 'Please enter a store name!',
+	},
+	slug: String,
+	description: {
+		type: String,
+		trim: true,
+	},
+	tags: [String],
+	created: {
+		type: Date,
+		default: Date.now,
+	},
+	location: {
+		type: {
+			type: String,
+			default: 'Point',
+		},
+		coordinates: [
+			{
+				type: Number,
+				required: 'You must supply coordinates!',
+			},
+		],
+		address: {
+			type: String,
+			required: 'You must supply an address!',
+		},
+	},
 });
 
-storeSchema.pre("save", function (next) {
-  // create new slug only if adding new name or modifying name/
-  if (!this.isModified("name")) {
-    return next();
-  }
+storeSchema.pre('save', function (next) {
+	// create new slug only if adding new name or modifying name/
+	if (!this.isModified('name')) {
+		return next();
+	}
 
-  this.slug = slug(this.name);
-  next();
+	this.slug = slug(this.name);
+	next();
 
-  // TODO: add code to eliminate redundant slugs
+	// TODO: add code to eliminate redundant slugs
 });
 
-module.exports = mongoose.model("Store", storeSchema);
+module.exports = mongoose.model('Store', storeSchema);
