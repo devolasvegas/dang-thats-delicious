c2ce6389795c29f088cfaf865c88e56289fd4686
devolasvegas <devondaviau@yahoo.ca>
2020-04-18 14:26:48 -0400

    add user registration and login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const User = mongoose.model('User');
+const promisify = require('es6-promisify');
 
 exports.loginForm = (req, res) => {
 	res.render('login', { title: 'Login' });
@@ -40,4 +42,11 @@ exports.validateRegister = (req, res, next) => {
 	}
 
 	next();
+};
+
+exports.register = async (req, res, next) => {
+	const user = new User({ email: req.body.email, name: req.body.name });
+	const register = promisify(User.register, User);
+	await register(user, req.body.password);
+	next(); // pass to authController.login
 };
