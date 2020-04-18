0efd7eee1eca3d4f9e2e58cee8f035f2f141e33d
devolasvegas <devondaviau@yahoo.ca>
2020-04-18 14:43:08 -0400

    add gravatar, add logout middleware, protect add stores route

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,3 +6,19 @@ exports.login = passport.authenticate('local', {
 	successRedirect: '/',
 	successFlash: 'You are now logged in! 😀',
 });
+
+exports.logout = (req, res) => {
+	req.logout();
+	req.flash('success', 'You are now logged out.');
+	res.redirect('/');
+};
+
+exports.isLoggedIn = (req, res, next) => {
+	// check if the user is authenticated
+	if (req.isAuthenticated()) {
+		return next();
+	}
+
+	req.flash('error', 'Oops! you must be logged in to do that! ☝');
+	res.redirect('/login');
+};
