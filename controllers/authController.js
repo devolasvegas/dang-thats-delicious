369905a7e9dff9fa495dfb6ec924b372f3a50444
devolasvegas <devondaviau@yahoo.ca>
2020-04-21 23:21:47 -0400

    start building out password reset functionaligy

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,7 @@
 const passport = require('passport');
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const User = mongoose.model('User');
 
 exports.login = passport.authenticate('local', {
 	failureRedirect: '/login',
@@ -20,5 +23,31 @@ exports.isLoggedIn = (req, res, next) => {
 	}
 
 	req.flash('error', 'Oops! you must be logged in to do that! ☝');
+	res.redirect('/login');
+};
+
+exports.forgot = async (req, res) => {
+	// 1. See if user exists
+	const user = await User.findOne({ email: req.body.email });
+
+	if (!user) {
+		req.flash('success', 'A password reset has been mailed to you.');
+		return res.redirect('/login');
+	}
+
+	// 2. Set reset token and expirty on their account
+	user.resetPasswordToken = crypto.randomBytes(20).toString('hex');
+	user.resetPasswordExpires = Date.now() + 3600000; // One hour from 'now'
+
+	await user.save();
+
+	// 3. Send email with token
+	const resetURL = `https://${req.headers.host}/account/reset/${user.resetPasswordToken}`;
+	req.flash(
+		'success',
+		`You have been emailed a password reset link ${resetURL}`
+	);
+
+	// 4. Redirect to login page
 	res.redirect('/login');
 };
